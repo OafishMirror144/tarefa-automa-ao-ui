e3b7b4ad12534bf276679e5279156b8b2a303538
oafish mirror <theparzival8@gmail.com>
2025-02-23 23:57:59 -0300

    tarefa de automaçao

--- cypress/e2e/exercicio-e2e.cy.js
+++ cypress/e2e/exercicio-e2e.cy.js
@@ -1,7 +1,9 @@
 /// <reference types="cypress" />
+import compraCompleta from "../support/page_objects/nome-funcionliada.page"
+
 
 context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
-  /*  Como cliente 
+     /*  Como cliente 
       Quero acessar a Loja EBAC 
       Para fazer um pedido de 4 produtos 
       Fazendo a escolha dos produtos
@@ -10,13 +12,89 @@ context('Exercicio - Testes End-to-end - Fluxo de pedido', () => {
       E validando minha compra ao final */
 
   beforeEach(() => {
-      cy.visit('/')
-  });
+    compraCompleta.visitarUrl() });
+
+      it('adicionar 4 produtos diferentes no carrinho', () => {
+        cy.fixture('produtos').then(dados =>{
+
+            compraCompleta.buscarProduto(dados[1].nomeProduto)
+            compraCompleta.addProdutoCarrinho(
+                 dados[1].tamanho,
+                 dados[1].cor,
+                 dados[1].quantidade)
+                 
+            cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+
+            compraCompleta.buscarProduto(dados[5].nomeProduto)
+            compraCompleta.addProdutoCarrinho(
+                 dados[5].tamanho,
+                 dados[5].cor,
+                 dados[5].quantidade)
+
+            cy.get('.woocommerce-message').should('contain', dados[5].nomeProduto)
+
+            compraCompleta.buscarProduto(dados[0].nomeProduto)
+
+            compraCompleta.addProdutoCarrinho(
+                 dados[0].tamanho,
+                 dados[0].cor,
+                 dados[0].quantidade)
+                 
+            cy.get('.woocommerce-message').should('contain', dados[0].nomeProduto)
+
+            compraCompleta.buscarProduto(dados[4].nomeProduto)
+            compraCompleta.addProdutoCarrinho(
+                dados[4].tamanho,
+                dados[4].cor,
+                dados[4].quantidade)
+                
+            cy.get('.woocommerce-message').should('contain', dados[4].nomeProduto)
+
+
+           cy.visit('carrinho')
+           cy.get('.checkout-button').click()
+           cy.get('.showlogin').click()
+           cy.get('#username').type("aluno_ebac@teste.com")
+           cy.get('.woocommerce-form > .form-row-last > label').type("teste@teste.com")
+           cy.get('.woocommerce-button').click()
+           cy.get('#terms').check()
+           cy.get('#place_order').click()
+
+           cy.get('.page-title').should('exist')
+
+
+
+        })
+    });
+
+
 
   it('Deve fazer um pedido na loja Ebac Shop de ponta a ponta', () => {
       //TODO: Coloque todo o fluxo de teste aqui, considerando as boas práticas e otimizações
-      
-  });
+      cy.fixture('produtos').then(dados =>{
+
+        compraCompleta.buscarProduto(dados[1].nomeProduto)
+        compraCompleta.addProdutoCarrinho(
+             dados[1].tamanho,
+             dados[1].cor,
+             dados[1].quantidade)
+             
+        cy.get('.woocommerce-message').should('contain', dados[1].nomeProduto)
+
+        cy.visit('carrinho')
+           cy.get('.checkout-button').click()
+           cy.get('.showlogin').click()
+           cy.get('#username').type("aluno_ebac@teste.com")
+           cy.get('.woocommerce-form > .form-row-last > label').type("teste@teste.com")
+           cy.get('.woocommerce-button').click()
+           cy.get('#terms').check()
+           cy.get('#place_order').click()
+
+           cy.get('.page-title').should('exist')
+            
+    })
+    });
 
 
-})
+});
--- cypress/support/page_objects/nome-funcionliada.page.js
+++ cypress/support/page_objects/nome-funcionliada.page.js
@@ -1,9 +1,26 @@
-class NomeClasse {
+class compraCompleta {
 
-    nomeMetodo(parametros ){
-        //ações do método
+    visitarUrl(){
+        cy.visit('produtos')
+    }
+    visitarUrlcarrinho(){
+        cy.visit('carrinho')
+    }
+    buscarProduto(nomeProduto){
+        cy.get('[name="s"]').eq(1).type(nomeProduto)
+        cy.get('.button-search').eq(1).click()
     }
 
+    PesquisarProdutos(nomeProduto){
+        cy.get('[name="s"]').eq(0).type(nomeProduto)
+        cy.get('.button-search').eq(0).click()
+    }
+    addProdutoCarrinho(tamanho, cor, quantidade){
+        cy.get('.button-variable-item-' + tamanho).click().click()    
+        cy.get(`.button-variable-item-${cor}`).click()
+        cy.get('.input-text').clear().type(quantidade)
+        cy.get('.single_add_to_cart_button').click()
+    }
 }
 
-export default new NomeClasse()
+export default new compraCompleta
